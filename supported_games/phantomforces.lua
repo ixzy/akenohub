6d7d776680b978d9ff8e68ee20d4cc8c501982a0
ixzy <68893555+ixzy@users.noreply.github.com>
2021-12-13 17:21:58 -0500

    Create phantomforces.lua

---
+++ supported_games/phantomforces.lua
@@ -0,0 +1,11 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+        local Window = Library.CreateLib("AkenoHubV2-[Phantom Forces]", "Synapse")
+     
+        -- MAIN
+        local Main = Window:NewTab("Main")
+        local MainSection = Main:NewSection("Main")
+        MainSection:NewButton("EvoV2","evo evo",
+          function(evo)
+            loadstring(game:HttpGet("https://projectevo.xyz/script/loader.lua"))()
+          end)
+    end
