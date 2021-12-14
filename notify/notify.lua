55a613a98ecb1196dfa5ebcde497caacec4944fd
ixzy <68893555+ixzy@users.noreply.github.com>
2021-12-14 18:18:25 -0500

    Create notify.lua

---
+++ notify/notify.lua
@@ -0,0 +1,10 @@
+local basic = "AkenoHub"
+function Notify(bruh,breh)
+    game:GetService("StarterGui"):SetCore(
+        "SendNotification",
+        {
+            Title = bruh,
+            Text = breh
+        }
+    )
+end
