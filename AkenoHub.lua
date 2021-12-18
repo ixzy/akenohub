f1a80c27d6cc03b72c70367e142401f19f3fd139
ixzy <68893555+ixzy@users.noreply.github.com>
2021-12-18 11:19:19 -0500

    Update AkenoHub.lua

--- AkenoHub.lua
+++ AkenoHub.lua
@@ -1,33 +1,42 @@
-local supported_games = {3956818381,5324597737,292439477,155615604}--dont delete this
+--notification start--
+local supported_games = {3956818381,5324597737,292439477,155615604,6875469709}--dont delete this
 
-if table.find(supported_games,game.PlaceId) then--dont delete this 
-    local ixzy = "Have Fun!!!!"
-    game:GetService("StarterGui"):SetCore(
-        "SendNotification",
-        {
-            Title = "Akeno Hub",
-            Text = ixzy
-        }
-    )
-    if game.PlaceId == 5324597737 then--dragon ball evolution
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/dbe.lua"))();
-    end
+if table.find(supported_games,game.PlaceId) then--dont delete this -- ok 
+  function notify(Title,Text,Duration)
+      game.StarterGui:SetCore("SendNotification", {
+          Title = Title,
+          Text = Text,
+          Duration = tonumber(Duration)
+      })
+  end
+  --loader start--
+  if game.PlaceId == 5324597737  then
+  notify("Akeno Hub", "Loading Dragon Ball Evolution", 6) 
+  wait(2)
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/dbe.lua'))
+  end
+  if game.PlaceId == 3956818381 then
+  notify("Akeno Hub", "Loading Ninja Legends", 6)   
+  wait(2)
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/ninjalegends.lua'))()
+  end
+  if game.PlaceId == 292439477 then
+  notify("Akeno Hub", "Loading Phantom Forces", 6)  
+  wait(2)
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/phantomforces.lua'))()
+  end
+  if game.PlaceId == 155615604 then
+  notify("Akeno Hub", "Loading Prison Life", 6)
+  wait(2)
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/prisonlife.lua'))()
+  end
+  if game.PlaceId == 6875469709 then
+    notify("Akeno Hub", "Loading Strongest Punch Simulator",6)
+       loadstring(game:HttpGet('https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/StrongestPunchSim.lua'))()
+  end
 
-    if game.PlaceId == 3956818381 then--ninja legends
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/ninjalegends.lua"))()
-    end
-    if game.PlaceId == 292439477 then--phantom forces 
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/phantomforces.lua"))()
-    end
-    if game.PlaceId == 155615604 then--prison life
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/ixzy/akenohub/main/supported_games/prisonlife.lua"))()
-    end
 else
-     game:GetService("StarterGui"):SetCore(
-    "SendNotification",
-    {
-        Title = "Akeno Hub",
-        Text = "We dont Support this game, You get universal gui"
-    })
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/ixzy/akenohub/main/Universal/Universal.lua"))()
-end
+  notify("Akeno Hub", "Loading Universal script", 6)  
+  wait(2)
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/ixzy/akenohub/main/Universal/Universal.lua'))()
+ end--loader end
